9610692812bf04719b23eae8083416ccad5dffdf
Marco Aurelio Lima <marco_aurelio029@hotmail.com>
2026-03-24 17:56:59 -0300

    feat: gerar cpf aleatório válido

--- errors.go
+++ errors.go
@@ -2,9 +2,15 @@ package cpf
 
 import "errors"
 
+// Declaração de erros específicos para validação de CPF
 var (
 	ErrorCPFVazio       = errors.New("CPF vazio")
 	ErrorCPFIncompleto  = errors.New("CPF com tamanho diferente de 11 dígitos")
 	ErrorCPFNaoNumerico = errors.New("CPF contém caracteres não numéricos")
 	ErrorCPFInvalido    = errors.New("CPF inválido")
+)
+
+// Declaração de erro específico para falha na geração de CPF aleatório
+var (
+	ErrorGerarCPFAleatorio = errors.New("não foi possível gerar um CPF aleatório válido após o número máximo de tentativas")
 )
